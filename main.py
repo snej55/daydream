e7b99d2e688ad838748701a32a3844a0c7c20575
snej55 <jenskromdijk@gmail.com>
2025-09-28 15:25:56 +0100

    Add dust

--- main.py
+++ main.py
@@ -93,10 +93,34 @@ class App:
         self.prompt = self.large_font.render("Press ENTER to start", True, (255, 255, 255))
         self.logo_text = self.large_font.render("System of a Cloud", True, (255, 255, 255))
 
+        self.kickup = []
+
+    def update_kickup(self, render_scroll):
+        # particle: [pos, vel, size, color]
+        for i, p in sorted(enumerate(self.kickup), reverse=True):
+            p[0][0] += p[1][0] * self.dt
+            if self.tile_map.solid_check(p[0]):
+                p[1][0] *= -0.8
+                p[1][1] *= 0.999
+
+            p[1][1] += 0.1 * self.dt
+            p[0][1] += p[1][1] * self.dt
+            if self.tile_map.solid_check(p[0]):
+                p[1][1] *= -0.8
+                p[1][0] *= 0.999
+
+            p[2] -= 0.1 * self.dt
+            if p[2] <= 0:
+                self.kickup.pop(i)
+            else:
+                color = pygame.Color(p[3][0], p[3][1], p[3][2], p[2] / 10 * 255)
+                self.screen.set_at((p[0][0] - render_scroll[0], p[0][1] - render_scroll[1]), color)
+
     def menu(self):
         self.screen.fill((0, 0, 0))
         self.screen.blit(self.prompt, (self.screen.get_width() // 2 - self.prompt.get_width() // 2, self.screen.get_height() // 2 - self.prompt.get_height() // 2))
         self.screen.blit(self.logo_text, (self.screen.get_width() // 2 - self.logo_text.get_width() // 2, self.screen.get_height() // 10 - self.logo_text.get_height() // 2))        
+    
     def start_level_transition(self, next_level):
         """Start the fade-out transition to a new level"""
         if self.transition_state == "none":
