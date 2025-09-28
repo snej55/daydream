a9e90c1c83ef48ecbb27749765fade73266c24bd
snej55 <jenskromdijk@gmail.com>
2025-09-28 10:07:55 +0100

    add font

--- src/tiles.py
+++ src/tiles.py
@@ -1,3 +1,4 @@
+import pygame
 from util import read_json
 
 TILE_SIZE = 8
@@ -10,6 +11,7 @@ class TileMap:
         self.app = app
         self.tile_map = {}
         self.off_grid = []
+        self.tile_size = TILE_SIZE
 
     def load(self, path):
         # open file
@@ -30,11 +32,27 @@ class TileMap:
         for tile in self.off_grid:
             tile['type'] = tile['type']
 
+    def tiles_around(self, pos):
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
+        for offset in OFFSETS:
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+            if check_loc in self.tile_map:
+                tiles.append(self.tile_map[check_loc])
+        return tiles
+
     def solid_check(self, pos):
         tile_loc = str(int(pos[0] // self.tile_size)) + ';' + str(int(pos[1] // self.tile_size))
         if tile_loc in self.tile_map:
             if self.tile_map[tile_loc]['type'] in PHYSICS_TILES:
                 return self.tile_map[tile_loc]
+    
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile['type'] in PHYSICS_TILES:
+                rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
+        return rects
 
     def draw(self, surf, scroll):
         for tile in self.off_grid:
